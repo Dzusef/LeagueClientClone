b5525b34287e01783d49f005b9bb850ad08dea39
Gooala <42656130+Gooala@users.noreply.github.com>
2020-04-28 11:12:36 +0200

    Update leaguesim.php

--- leaguesim.php
+++ leaguesim.php
@@ -43,18 +43,66 @@ function assistsCalculator() {
     }
     return $assists;
 }
+
+function gameTimeCalculator() {
+    $probability = rand(0, 100);
+    $avgGameTimeLow = rand(15, 25);
+    $avgGameTimeMed = rand(26,  40);
+    $avgGameTimeHigh = rand(41, 50);
+    $avgGameTimeUltraHigh = rand(51, 70);
+    if ($probability < 15) {
+        $gameTime = $avgGameTimeLow;
+    } elseif ($probability > 85) {
+        if ($probability > 95) {
+            $gameTime = $avgGameTimeUltraHigh;
+        } else {
+            $gameTime = $avgGameTimeHigh;
+        }
+    } else {
+        $gameTime = $avgGameTimeMed;
+    }
+    return $gameTime;
+}
+
+function avgCSCalculator() {
+    $probability = rand(0, 100);
+    $avgCSLow = rand(90, 120);
+    $avgCSMid = rand(121, 200);
+    $avgCSHigh = rand(200, 250);
+    $avgCSUltraHigh = rand(251, 300);
+    $supProbability = rand(0, 100);
+    $supAvgCS = rand(20, 40);
+    if ($supProbability < 20) {
+        $gameCS = $supAvgCS . " (sup)";
+    } else {
+        if ($probability < 20) {
+            $gameCS = $avgCSLow;
+        } elseif ($probability > 80) {
+            if ($probability > 95) {
+                $gameCS = $avgCSUltraHigh;
+            } else {
+                $gameCS = $avgCSHigh;
+            }
+        } else {
+            $gameCS = $avgCSMid;
+        }
+    }
+    return $gameCS;
+}
 ?>
 
 <!doctype html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <title>text demo</title>
-        <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
-    </head>
-    <body>
-        <?php
-        echo "KDA: " . killsCalculator() . "/" . deathsCalculator() . "/" . assistsCalculator();
-        ?>
-    </body>
+<head>
+    <meta charset="utf-8">
+    <title>text demo</title>
+    <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+</head>
+<body>
+<?php
+echo "KDA: " . killsCalculator() . "/" . deathsCalculator() . "/" . assistsCalculator();
+echo "<br>Creep Score: " .avgCSCalculator();
+echo "<br>Duration: " . gameTimeCalculator() . " min";
+?>
+</body>
 </html>
