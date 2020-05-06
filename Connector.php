1e5d1f2f43909f0faefb041ac7d45ee6f11e4ea4
Gooala <42656130+Gooala@users.noreply.github.com>
2020-05-06 12:57:03 +0200

    Update Connector.php

--- Connector.php
+++ Connector.php
@@ -1,4 +1,4 @@
-<?php $link = mysqli_connect("localhost", "User", "UqyusUBDFESo6LxI", "leaguedbcopy");
+<?php $link = mysqli_connect("localhost", "root", "", "leaguedbcopy");
 session_start();
 if($link === false){
    die("ERROR: Could not connect. " . mysqli_connect_error());
