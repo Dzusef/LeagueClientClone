843acdef5c2f8059df6dec2e91b4d901ca2fb340
Dzusef <62593074+Dzusef@users.noreply.github.com>
2020-05-26 20:53:12 +0200

    Update Register.php

--- Register.php
+++ Register.php
@@ -1,28 +1,31 @@
-<?php function LoginCheck() {
+<?php function Register() {
   include("Connector.php");
-  $sql = "SELECT Name,Password FROM customeraccounts";
+  $sql = "SELECT Name,Email FROM customeraccounts";
   if($result = mysqli_query($link, $sql)) {
 
-    if((isset($_POST['uname']) != "") && ((isset($_POST['password'])) != "") ) {
-      $query = mysqli_query($link,"SELECT Name,Password FROM customeraccounts WHERE Name='".$_POST['uname']."' AND Password='".$_POST['password']."'");
-      $_SESSION['username'] = $_POST['uname'];
-      $_SESSION['password'] = $_POST['password'];
+    if((isset($_POST['uname']) != "") && ((isset($_POST['email'])) != "")) {
 
+      $query = mysqli_query($link,"SELECT Name,Email FROM customeraccounts WHERE Name='".$_POST['uname']."' OR Email='".$_POST['email']."'");
       if (mysqli_num_rows($query) == 1) {
-        $sqlStatus = "UPDATE customeraccounts SET Status='Online' WHERE Name='".$_SESSION['username']."'";
-
-        if ($link->query($sqlStatus)) {
-            echo "Record updated successfully";
-        } else {
-            echo "Error updating record: " . $link->error;
-        }
-        header('Location: LoggedIn.php');
-        exit;
+        echo "<br>This username or email already exists, try again.";
+        unset($_POST['uname']);
+        unset($_POST['email']);
 
       } else {
-          echo "<br>Incorrect";
+          $sqlCreate = "INSERT INTO customeraccounts VALUES (NULL, '".$_POST['uname']."', '".$_POST['password']."', '".$_POST['email']."', 0, 'Online')";
+          if ($link->query($sqlCreate)) {
+              echo "<br>Your account has been created!";
+              $_SESSION['username'] = $_POST['uname'];
+              $_SESSION['password'] = $_POST['password'];
+              unset($_POST['uname']);
+              unset($_POST['email']);
+              header('Location: LoggedIn.php');
+              exit;
+          } else {
+              echo "Error updating record: " . $link->error;
+          }
+        }
       }
-    }
   } else {
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
   }
@@ -40,25 +43,26 @@
           <legend>LoL Register</legend>
             <ul>
               <li>
-                <label for="uname">*Username</label>
+                <label for="uname">Username</label>
                 <input type="uname" name="uname">
               </li>
               <li>
-                <label for="password">*Password</label>
+                <label for="password">Password</label>
                 <input type="password" name="password">
               </li>
               <li>
-                <label for="password">*Retype Password</label>
-                <input type="password" name="password">
+                <label for="retype">Retype</label>
+                <input type="password" name="retype">
               </li>
               <li>
-                <label for="password">*Email</label>
+                <label for="email">Email</label>
                 <input type="email" name="email">
               </li>
             </ul>
           </fieldset>
+        <input type="submit" name="login" value="Login" formaction='Login.php'>
         <input type="submit" name="Register" value="Register">
       </form>
-      <?php LoginCheck() ?>
+      <?php Register() ?>
    </body>
 </html>
